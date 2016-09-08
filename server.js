3a81aa8d0e9ab43a4a9491cb7c39db8bbf3b6656
Anthony Re <a127@umbc.edu>
2016-09-08 19:42:51 -0400

    commiting before file structure changes

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ var debug = require('debug')('::http');
 const HOST = process.env.host || '0.0.0.0';
 const PORT = process.env.port || 3000;
 
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+  return res.render('index');  
+});
+
 app.listen(PORT, function() {
   debug("App is listening on Port %s", PORT);
 });
