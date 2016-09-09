c1b2e2efbf4563cad0168b4a4314cff200018e70
Anthony Re <a127@umbc.edu>
2016-09-08 22:32:19 -0400

    removed extraneous dependencies from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,15 @@ function stub() {
 }
 
 gulp.task('scripts', function bundle() {
-  var watchify = require('watchify');
   var browserify = require('browserify');
- 
   var bundleStream = browserify('./public/src/js/index.js').bundle();
   
-  var source      = require('vinyl-source-stream'),
-      streamify   = require('gulp-streamify'),
-      uglify      = require('gulp-uglify'),
-      rename      = require('gulp-rename')
+  var source      = require('vinyl-source-stream');
   
-  bundleStream
+  return bundleStream
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('./public/dist/js'));
+  
 });
 
 gulp.task('watch', function() {
